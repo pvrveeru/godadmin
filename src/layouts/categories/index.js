8f6c4ae0e740abce019753c2ab6afcb7665e0ebc
Veeru <veeru.p@idensys-tech.com>
2025-04-16 11:44:56 +0530

    ui

--- src/layouts/categories/index.js
+++ src/layouts/categories/index.js
@@ -215,7 +215,9 @@ const Categories = () => {
 
   return (
     <DashboardLayout>
-      <DashboardNavbar />
+      <div className="hide-on-desktop">
+        <DashboardNavbar />
+      </div>
       <MDBox pt={3} pb={3}>
         <Grid container spacing={6}>
           <Grid item xs={12}>
